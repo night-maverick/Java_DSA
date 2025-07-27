baee8e9da42b4182cfbb4f298b19c8a41b46d00f
Deepayan Chakravarti <deepayan.chakravarti@gmail.com>
2025-07-27 15:38:04 +0530

    Week6 updated

---
+++ WagmiJava/Week6/ReverseString.java
@@ -0,0 +1,21 @@
+package WagmiJava.Week6;
+//https://leetcode.com/problems/reverse-string/description/
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length -1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+    }
+}
